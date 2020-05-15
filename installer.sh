dee803e9fac6d7e5c34aa60d2d0eabbbeba0b0df
alexelite <alex.elite@gmail.com>
2020-05-16 00:05:54 +0300

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,24 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-function error { echo -e "[Error] $*"; exit 1; }
-function warn  { echo -e "[Warning] $*"; }
-
-warn "This installer is no longer supported."
-warn ""
-warn "Home Assistant might work today, tomorrow maybe not."
-warn ""
-warn "If you want more control over your own system, run"
-warn "Home Assistant as a VM or run Home Assistant Core"
-warn "via a Docker container."
-warn ""
-echo 'Please typ "not supported" to continue this installation'
-read x
-if [ "$x" != "not supported" ]
-then
-  echo "OK, bye!"
-  exit 1
-fi
 
 ARCH=$(uname -m)
 DOCKER_BINARY=/usr/bin/docker
@@ -37,7 +19,7 @@ command -v systemctl > /dev/null 2>&1 || error "Only systemd is supported!"
 command -v docker > /dev/null 2>&1 || error "Please install docker first"
 command -v jq > /dev/null 2>&1 || error "Please install jq first"
 command -v curl > /dev/null 2>&1 || error "Please install curl first"
-command -v avahi-daemon > /dev/null 2>&1 || error "Please install avahi first"
+#command -v avahi-daemon > /dev/null 2>&1 || error "Please install avahi first"
 command -v dbus-daemon > /dev/null 2>&1 || error "Please install dbus first"
 command -v nmcli > /dev/null 2>&1 || warn "No NetworkManager support on host."
 command -v apparmor_parser > /dev/null 2>&1 || warn "No AppArmor support on host."
